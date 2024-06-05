b1aa2f0119b9a646e97d12fcb92d8a5d2b01f2e0
Anafer23 <analiafer2003@gmail.com>
2024-06-04 21:45:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("Bienvenido a mi portfolio!")
+//alert("Bienvenido a mi portfolio!")
 const nombre = prompt("Cómo te llamas?")
 alert("Hola " + nombre + "! Bienvenido a mi portfolio!")
 // Array de frases
@@ -15,14 +15,11 @@ const frases = [
 // Función para generar una frase aleatoria
 function generarFrase() {
     // Genera un índice aleatorio dentro del rango de la longitud del array de frases
-    const frases =  {
-
-};   
-const indice = Math.floor(Math.random() * frases.length);
+    const indice = Math.floor(Math.random() * frases.length);
     // Obtiene la frase correspondiente al índice generado
-const fraseAleatoria = frases[indice];
+    const fraseAleatoria = frases[indice];
     // Muestra la frase en el footer
-document.getElementById("frase").innerText = fraseAleatoria;
+    document.getElementById("frase").innerText = fraseAleatoria;
 }
 // Agrega un listener al botón para ejecutar la función generarFrase cuando se haga clic
 document.getElementById("btn-generar").addEventListener("click", generarFrase);
